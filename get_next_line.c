6d0ff840cbcfc095edbf9cb6faa6746d132a6fa8
Aleksandra Heinanen <aleksandra.heinanen@gmail.com>
2023-11-24 17:04:03 +0200

    just testing how it works

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: aheinane <aheinane@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 13:49:53 by aheinane          #+#    #+#             */
-/*   Updated: 2023/11/24 14:36:33 by aheinane         ###   ########.fr       */
+/*   Updated: 2023/11/24 16:24:05 by aheinane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,23 @@
 char *get_next_line(int fd)
 {
 	static char* s;
+	char buffer[100];
+	size_t nbytes;
+	ssize_t bytes_read;
+	int i = 0;
 
-	if (fd < 0)
-		return NULL;
-	read(fd, s, BUFFER_SIZE );
+	if (fd < 0 || nbytes <= 0)
+		return (NULL);
+	nbytes=  sizeof(buffer);
+	bytes_read	 = read(fd,buffer, nbytes);
+	if (bytes_read < 0)
+		return (-1);
+	buffer[nbytes] = '\0';
+	while (s != '\n')
+	{
+		s++;
+	}
+	s = ft_strjoin(s, buffer);
+	s = malloc (sizeof(s));
+	free(s);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,50 @@
 /*   By: aheinane <aheinane@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 13:51:09 by aheinane          #+#    #+#             */
-/*   Updated: 2023/11/24 13:51:10 by aheinane         ###   ########.fr       */
+/*   Updated: 2023/11/24 15:23:24 by aheinane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+
+size_t	ft_strlen(const char *str )
+{
+	size_t	i;
+	size_t	count;
+
+	count = 0;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+		count++;
+	}
+	return (count);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	size_t	j;
+	char	*new;
+
+	i = 0;
+	j = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	new = (char *) malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!new)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		new[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		new[i + j] = s2[j];
+		j++;
+	}
+	new[i + j] = '\0';
+	return (new);
+}
