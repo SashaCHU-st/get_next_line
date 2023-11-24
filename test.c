ef09ed2bad32e9c6778c8df41469a7b071a6bd37
SashaCHU-st <72024418+SashaCHU-st@users.noreply.github.com>
2023-11-24 17:07:58 +0200

    Add files via upload

---
+++ test.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+char *test(int fd)
+{
+	int i = 0;
+	static char buffer[15];
+	read(fd, buffer, 14);
+	buffer[14] = '\0';
+	while (buffer[i] != '\n')
+		{
+			printf("%c",buffer[i]);
+			i++;
+		}
+	printf("\n");
+	return(buffer);
+}
+
+int main ()
+{
+	int fd;
+	fd = open("test_delete.txt", O_RDONLY);
+	test(fd);
+	close(fd);
+	return (0);
+}
