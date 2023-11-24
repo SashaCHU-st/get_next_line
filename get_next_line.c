c6c0b7ff751f69bf23b754bbb6849ca4e50e9f46
Aleksandra Heinanen <aleksandra.heinanen@gmail.com>
2023-11-24 15:04:12 +0200

    new

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,18 @@
 /*   By: aheinane <aheinane@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 13:49:53 by aheinane          #+#    #+#             */
-/*   Updated: 2023/11/24 13:50:23 by aheinane         ###   ########.fr       */
+/*   Updated: 2023/11/24 14:36:33 by aheinane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include <unistd.h>
 
 char *get_next_line(int fd)
 {
-	static char *;
+	static char* s;
+
+	if (fd < 0)
+		return NULL;
+	read(fd, s, BUFFER_SIZE );
 }
