c98dd7bc15c23710f33c9d8fc23fe9e7e289a33d
Aleksandra Heinanen <aleksandra.heinanen@gmail.com>
2023-12-12 12:01:16 +0200

    trying smth to test how it works

--- get_next_line.c
+++ get_next_line.c
@@ -6,33 +6,81 @@
 /*   By: aheinane <aheinane@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 13:49:53 by aheinane          #+#    #+#             */
-/*   Updated: 2023/12/10 10:22:40 by aheinane         ###   ########.fr       */
+/*   Updated: 2023/12/11 16:53:06 by aheinane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+//#include <unistd.h>
+#include <sys/types.h>
+#include "get_next_line.h"
 
-char *get_next_line(int fd)
+//  nujna function kotoraya chitaet dlinu buffera BUFFER_SIZE=42 (ft_read) 
+//i nahodit v nem '\n' pri pomoshi ft_strchr
+// vydelayet malloc dlya dliny +1
+// esli malloc 0 return 0
+// 
+
+//polsle togo kak nashla '\n' kopiruet vse do '\n'
+// pri pomoshi strdup v novuuperemenuy line i return(line)
+
+//udalayet ne nujnoe do '\n' pri pomoshi '\n'
+
+// isppolsuya static variable sohranyaetsy gde zakonchilo i 
+//soedinyaet so sledyushim string do '\n' ft_strjoin
+
+
+char	*ft_read(int fd, char *storage)
 {
-	static char* s;
-	char BUFFER_SIZE[100];
-	size_t nbytes;
-	ssize_t bytes_read;
-	int i = 0;
+	int		new;
+	char	*newline;
 
-	if (fd < 0 || nbytes <= 0)
-		return (NULL);
-	nbytes =  sizeof(BUFFER_SIZE);
-	bytes_read	 = read(fd ,BUFFER_SIZE, nbytes);
-	if (bytes_read < 0)
-		return (-1);
-	BUFFER_SIZE[nbytes] = '\0';
-	while (s != '\n')
+	new = 0;
+	new = malloc(sizeof(new));// malloc mesto dlya novogo stroki
+	if (!new)
+		return(NULL);
+	while (new != '\n')// esli vsrechaet 
 	{
-		s++;
+		new = read(fd, storage, BUFFER_SIZE);/// chitaet v fd po kol-vu BUFFER_SIZE and store in storage 
+		newline = ft_strjoin(newline, storage);
+		newline[new]= '\0';
 	}
-	s = ft_strjoin(s, BUFFER_SIZE);
-	s = malloc (sizeof(s));
+	newline = malloc(sizeof(newline + 1));
+	if (!newline)
+		return (NULL);
+	return (newline);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*s;
+	char		*new;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	//s = malloc(sizeof(BUFFER_SIZE + 1));
+	//if (!s)
+///return (NULL);
+	s = ft_read (fd, s);
+	if (s < 0)
+		return (NULL);
+	while (*s != '\n')
+	{
+		*s++;
+	}
+	// s = ft_strjoin(s, BUFFER_SIZE);
+	// s = malloc (sizeof(s));
 	free(s);
+	return(s);
+}
+int main ()
+{
+	int fd;
+	fd = open("test_delete.txt", O_RDONLY);
+	get_next_line(fd);
+	get_next_line(fd);
+	close(fd);
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,24 @@
 /*   By: aheinane <aheinane@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 13:52:59 by aheinane          #+#    #+#             */
-/*   Updated: 2023/12/10 10:14:47 by aheinane         ###   ########.fr       */
+/*   Updated: 2023/12/11 11:37:50 by aheinane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFFER_SIZE = 42
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 char *get_next_line(int fd);
+size_t	ft_strlen(const char *str );
+char	*ft_strjoin(char const *s1, char const *s2);
+
 
 #endif
